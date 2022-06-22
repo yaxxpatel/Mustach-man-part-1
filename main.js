ff15b7840740a0b1f54bc1f77ba4dd77929660bd
yaxxpatel <88316870+yaxxpatel@users.noreply.github.com>
2022-06-22 20:58:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(300 , 300);
+    canvas.center();
+}
+
+function draw() {}
+
+    function take_snapshot()
+    {
+        save('myFilterImage.png')
+    }
+
+    Webcam.set 
